9518d953873d2a8eafaae0010a0b6a1887985901
A-Arham <786arhamabdul@gmail.com>
2025-07-23 15:11:46 +0500

    Adding POST Response to the API project

--- Controllers/TeamsController.cs
+++ Controllers/TeamsController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using footballLeagueApi.Models;
 using FootballLeagueConsoleApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FootballLeagueApi.Controllers
 {
@@ -14,11 +15,32 @@ namespace FootballLeagueApi.Controllers
             _context = context;
         }
 
+        // GET: api/teams
         [HttpGet]
         public IActionResult GetTeams()
         {
             var teams = _context.Teams.ToList();
             return Ok(teams);
         }
+
+        [HttpPost]
+        public IActionResult CreateTeam([FromBody] TeamCreateDto teamDto)
+        {
+            if (teamDto == null || string.IsNullOrWhiteSpace(teamDto.Name) || string.IsNullOrWhiteSpace(teamDto.City))
+                return BadRequest("Name and City are required.");
+
+            var team = new Team
+            {
+                Name = teamDto.Name,
+                City = teamDto.City
+            };
+
+            _context.Teams.Add(team);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetTeams), new { id = team.TeamId }, team);
+        }
+
+
     }
 }
---
+++ Models/TeamCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace footballLeagueApi.Models
+{
+    public class TeamCreateDto
+    {
+        public string Name { get; set; } = null!;
+        public string City { get; set; } = null!;
+    }
+
+}
