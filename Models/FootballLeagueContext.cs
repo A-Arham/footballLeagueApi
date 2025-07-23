1ddcfbef33e0fe9e253642b3e0e2010abc762688
A-Arham <786arhamabdul@gmail.com>
2025-07-23 14:49:08 +0500

    Bug fixes

--- Models/FootballLeagueContext.cs
+++ Models/FootballLeagueContext.cs
@@ -15,15 +15,15 @@ public partial class FootballLeagueContext : DbContext
     {
     }
 
-    public virtual DbSet<Match> Matches { get; set; }
+    public DbSet<Match> Matches { get; set; }
 
-    public virtual DbSet<Player> Players { get; set; }
+    public DbSet<Player> Players { get; set; }
 
-    public virtual DbSet<Team> Teams { get; set; }
+    public DbSet<Team> Teams { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=127.0.0.1,1433;Database=FootballLeague;Trusted_Connection=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=AARHAM\\SQLEXPRESS;Database=FootballLeague;Trusted_Connection=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
